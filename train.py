3dffb6f836d88f6f6d4a9b16711fd6d6d2a80f07
Rana Edress <s-rana.hamdy@zewailcity.edu.eg>
2026-04-14 00:05:34 +0200

    Initial commit with workflow

---
+++ train.py
@@ -0,0 +1,13 @@
+import time
+
+def main():
+    print("Starting training")
+
+    for i in range(5):
+        print(f"Epoch {i+1}/5 ")
+        time.sleep(1)
+
+    print("Training is completed")
+
+if __name__ == "__main__":
+    main()
