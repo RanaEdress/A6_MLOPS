837b42ac3ad73a19e3cc094c6d4ba7141a9788e2
Rana Edress <s-rana.hamdy@zewailcity.edu.eg>
2026-04-14 00:09:24 +0200

    Test failure [run-train]

--- train.py
+++ train.py
@@ -3,13 +3,11 @@ import time
 def main():
     print("Starting training")
 
-    for i in range(5):
-        print(f"Epoch {i+1}/5 ")
+    for i in range(3):
+        print(f"Epoch {i+1}")
         time.sleep(1)
 
-    print("Training is completed")
+    raise Exception("Intentional failure")
 
 if __name__ == "__main__":
     main()
-
-# run training
